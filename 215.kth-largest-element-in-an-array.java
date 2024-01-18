8b3ec90c96663282f4ba0c1897a4c538a91bb003
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2024-01-18 16:26:44 -0500

    Optimize Kth Largest Element problem

--- 215.kth-largest-element-in-an-array.java
+++ 215.kth-largest-element-in-an-array.java
@@ -9,39 +9,22 @@ import java.util.*;
 // @lc code=start
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
-
-        for (int i = 0; i < nums.length / 2; i++) {
-            pq1.add(nums[i]);
-        }
-
-        for (int i = nums.length / 2; i < nums.length; i++) {
-            pq2.add(nums[i]);
-        }
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int counter = 0;
-        Integer current = null;
 
-        while (!pq1.isEmpty() || !pq2.isEmpty()) {
-            if (!pq1.isEmpty() && !pq2.isEmpty()) {
-                if (pq1.peek() > pq2.peek()) {
-                    current = pq1.poll();
-                } else {
-                    current = pq2.poll();
-                }
-            } else if (!pq1.isEmpty() && pq2.isEmpty()) {
-                current = pq1.poll();
-            } else if (pq1.isEmpty() && !pq2.isEmpty()) {
-                current = pq2.poll();
-            }
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(nums[i]);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int current = pq.poll();
             counter++;
 
-            if (counter == k) {
+            if(counter == k) {
                 return current;
             }
         }
-        return current;
+        return Integer.MIN_VALUE;
     }
 }
 // @lc code=end
