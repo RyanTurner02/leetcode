3fba8df4decdee7090f9969327d02382b43988cc
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2024-04-09 16:17:10 -0400

    Solve optimal first bad version problem

--- 278.first-bad-version.java
+++ 278.first-bad-version.java
@@ -10,7 +10,20 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        
+        int low = 1;
+        int high = n;
+        int mid = 0;
+
+        while(low < high) {
+            mid = low + (high - low) / 2;
+            
+            if(isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
     }
 }
 // @lc code=end
