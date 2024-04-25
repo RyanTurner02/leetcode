e8c5df808b0a1896c99326f89e8360b23fa37b74
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2024-04-25 12:32:37 -0400

    Solve Contains Duplicate

--- 217.contains-duplicate.java
+++ 217.contains-duplicate.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /*
  * @lc app=leetcode id=217 lang=java
  *
@@ -7,8 +9,17 @@
 // @lc code=start
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
         
+        for(int i = 0; i < nums.length; i++) {
+            int curr = nums[i];
+
+            if(hs.contains(curr)) {
+                return true;
+            }
+            hs.add(curr);
+        }
+        return false;
     }
 }
 // @lc code=end
-
